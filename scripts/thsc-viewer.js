8d9812ce471861a82f3ddf41e5ff9f84f30680c9
thsconline <thsconline@users.noreply.github.com>
2018-02-01 00:50:43 +1100

    Fix Critical Issue

--- scripts/thsc-viewer.js
+++ scripts/thsc-viewer.js
@@ -1139,6 +1139,17 @@ document.write("<style>.drive-viewer-popout-button{display:none;}</style><iframe
 document.write("</body></html>");
 
 
+} 
+else {
+document.write("<a id=\"pdflink\" class=\"border\" href=\""+downloadlk+"\" target=\"_blank\">Download PDF<\/a>&nbsp;&nbsp;");
+document.write("<a id=\"texlink\" class=\"border\" href=\""+downloadtex+"\" target=\"_blank\">Download TEX<\/a>&nbsp;&nbsp;");
+document.write("&nbsp;&nbsp;<a class=\"border\" href=\""+tags+"\">Close &#215;<\/a>&nbsp;&nbsp;</span></span></span><br>");
+document.write("<style>.drive-viewer-popout-button{display:none;}</style><iframe style=\"width:98%; height:95%;\" height=\"95%\" id=\"mediaplayer\" src=\""+downloadlk+"\"><noscript>&nbsp;Enable Javascript to Load File<\/noscript><\/iframe>");
+document.write("</body></html>");
+
+
+}	
+
 }
 break;
 
