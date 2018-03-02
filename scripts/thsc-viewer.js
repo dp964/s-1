0c1555f4618d6af79d06c141d41ef73404526be6
thsconline <thsconline@users.noreply.github.com>
2018-03-03 10:51:42 +1100

    Viewer.js

--- scripts/thsc-viewer.js
+++ scripts/thsc-viewer.js
@@ -4,7 +4,7 @@ function writefile(myobject)
 {
 var idxq=myobject.fileref;
 var viewlkq="https:\/\/drive.google.com\/file\/d\/"+myobject.fileref+"\/preview";
-window.addEventListener("load",function (e) {init(e,viewlkq)},false);
+document.addEventListener("load",function (e) {init(e,viewlkq)},false);
 
     }
 	function init(a,b)
