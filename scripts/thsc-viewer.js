20a99dca542919def757fd38c4398e12f59e0a65
thsconline <thsconline@users.noreply.github.com>
2018-03-24 02:17:32 +1100

    Update

--- scripts/thsc-viewer.js
+++ scripts/thsc-viewer.js
@@ -9,7 +9,7 @@ var downloadlka="https:\/\/drive.google.com\/uc?export=download&id="+idxq;
 setTimeout(function(){document.getElementById("mediaplayer").src=b; 
 document.getElementById("mediaplayer").id="medialoaded"}, 1)
 var titlex=getParameterByName('n');
-fbox="<div class=\"overlay-content\"><span style=\"float:right;\"><a class=\"border\" href=\"#ui-blank\" onclick=\"closefbox()\">Close &#215;</a></span>"
+fbox="<span style=\"float:right;\"><a class=\"border\" href=\"#ui-blank\" onclick=\"closefbox()\">Close &#215;</a></span><div class=\"overlay-content\">"
 fbox="<b>"+titlex+"</b><a href=\"#\">test link 1</a><a href=\"#\">test link 2</a><a href=\"#\">test link 3</a></div>"
 
 
