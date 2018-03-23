d7b5c510b97aa013abda2dee3357bb7947fde5c2
thsconline <thsconline@users.noreply.github.com>
2018-03-24 01:16:20 +1100

    Download Files Update!

--- scripts/thsc-viewer.js
+++ scripts/thsc-viewer.js
@@ -4,11 +4,11 @@ function writefile(myobject)
 {
 var idxq=myobject.fileref;
 var b="https:\/\/drive.google.com\/file\/d\/"+myobject.fileref+"\/preview";
-var downloadlink="https:\/\/drive.google.com\/uc?export=download&id="+idxq;
+var downloadlink="https:\/\/drive.google.com\/uc?export=download&id="+idx;
 
 setTimeout(function(){document.getElementById("mediaplayer").src=b; 
 document.getElementById("mediaplayer").id="medialoaded"}, 1)
-setTimeout(function(){document.getElementsByClassName("downloadlk")[0].innerHTML="<a class=\"border\" href=\""+downloadlk+"\" target=\"_blank\">Download File<\/a>&nbsp;&nbsp;"}, 2)
+setTimeout(function(){document.getElementById("downloadlink").innerHTML="<a class=\"border\" href=\""+downloadlk+"\" target=\"_blank\">Download File<\/a>&nbsp;&nbsp;"}, 2)
     }
 
 
@@ -1041,7 +1041,7 @@ document.write("<span class=\"nmob\"><a class=\"border\" href=\"http://community
 // document.write("<span class=\"nmob\"><a class=\"border\" href=\"http://community.boredofstudies.org/newthread.php?do=newthread&f="+forumlk+"\" target=\"_blank\">Ask Question / Discuss<\/a>&nbsp;&nbsp;</span>");
 }
 // document.write("<a class=\"border\" href=\""+downloadlk+"\" target=\"_blank\">Download PDF<\/a>&nbsp;&nbsp;");
-document.write("<span class=\"downloadlk\"></span>");
+document.write("<span id=\"downloadlk\"></span>");
 }
 // document.write("<a class=\"border\" href=\"/s/?login\" target=\"_blank\">BETA Login/Register<\/a>&nbsp;&nbsp;");
 // document.write("<a class=\"border\" href=\"/s/?download"+searchq.substr(5)+"\" target=\"_blank\">BETA Download<\/a>&nbsp;&nbsp;");
