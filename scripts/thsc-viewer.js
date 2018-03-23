b1861f0891685e65671a1714dd9a92660c0451a2
thsconline <thsconline@users.noreply.github.com>
2018-03-24 01:07:33 +1100

    Update

--- scripts/thsc-viewer.js
+++ scripts/thsc-viewer.js
@@ -7,7 +7,6 @@ var b="https:\/\/drive.google.com\/file\/d\/"+myobject.fileref+"\/preview";
 var downloadlink="https:\/\/drive.google.com\/uc?export=download&id="+idx;
 
 setTimeout(function(){document.getElementById("mediaplayer").src=b; 
-
 document.getElementById("downloadlk").innerHTML="<a class=\"border\" href=\""+downloadlk+"\" target=\"_blank\">Download File<\/a>&nbsp;&nbsp;"
 document.getElementById("mediaplayer").id="medialoaded"}, 1)
 
