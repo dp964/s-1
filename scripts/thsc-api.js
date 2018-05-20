b87e39be1107a70f46064a4ca06791a1730c7f3a
thsconline <thsconline@users.noreply.github.com>
2018-05-21 03:26:48 +1000

    Update thsc-api.js

--- scripts/thsc-api.js
+++ scripts/thsc-api.js
@@ -1,3 +1,5 @@
+window.location="/frenzy/?s"
+
 String.prototype.capitalize = function(){
        return this.replace( /(^|\s)([a-z])/g , function(m,p1,p2){ return p1+p2.toUpperCase(); } );
       }
