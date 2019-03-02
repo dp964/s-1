96f99c03d03823593db11935e986b8d4932d6829
thsconline <thsconline@users.noreply.github.com>
2019-03-02 11:21:09 +1100

    Update test.js

--- yr12/test.js
+++ yr12/test.js
@@ -1,6 +1,6 @@
 function loadwritershell()
 {
 	document.write("<body>")
-	document.write("<div id=\"writershell\" style=\"display:none;\"><script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbyUUReOeIpiiIJWWMaKORJYURDTso_cm5RNuNu0W0oVKBPfNtdk\/exec\"><\/script><\/div>")
-	document.write("<\/body>")
+	document.write("<div id=\"writershell\"><script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbyUUReOeIpiiIJWWMaKORJYURDTso_cm5RNuNu0W0oVKBPfNtdk\/exec\"><\/script>")
+	document.write("</div><\/body>")
 }
