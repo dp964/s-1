47c520336e3a41c445ad04b8739b1f4b217f79b8
thsconline <thsconline@users.noreply.github.com>
2019-03-02 12:32:48 +1100

    Update thsc-api.js

--- scripts/thsc-api.js
+++ scripts/thsc-api.js
@@ -2,7 +2,7 @@ function loadshell()
 {	
 	var folder = document.getElementById("shell").getAttribute("data-hash");
 	var fname = document.getElementById("shell").getAttribute("data-filename");
-	document.write("<body><div><script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbyUUReOeIpiiIJWWMaKORJYURDTso_cm5RNuNu0W0oVKBPfNtdk\/exec?serve="+folder+"&filename="+fname+"&prefix=shell\"\"><\/script></div></body>";
+	document.write("<body><div><script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbyUUReOeIpiiIJWWMaKORJYURDTso_cm5RNuNu0W0oVKBPfNtdk\/exec?serve="+folder+"&filename="+fname+"&prefix=shell\"\"><\/script></div></body>");
 
 }
 
