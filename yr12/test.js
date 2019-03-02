8147f02a6b920f31963bdb7b86481cf6db53d79a
thsconline <thsconline@users.noreply.github.com>
2019-03-02 11:52:24 +1100

    Update test.js

--- yr12/test.js
+++ yr12/test.js
@@ -2,10 +2,8 @@ function loadshell()
 {
 	var folder = document.getElementById("shell").getAttribute("data-hash");
 	var fname = document.getElementById("shell").getAttribute("data-filename");
-	
-	
-	
-	document.getElementsByTagName("shell")[0].innerHTML = "<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbyUUReOeIpiiIJWWMaKORJYURDTso_cm5RNuNu0W0oVKBPfNtdk\/exec\"><\/script>"
+
+	document.getElementsByTagName("shell")[0].innerHTML = "<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbyUUReOeIpiiIJWWMaKORJYURDTso_cm5RNuNu0W0oVKBPfNtdk\/exec?serve="+folder+"&filename="+fname+"&prefix=shell\"\"><\/script>"
 }
 function shell(myobject)
 {
