9b6a594abc2f965f871d58c1e9aade6cbf425ec9
thsconline <thsconline@users.noreply.github.com>
2019-03-02 11:19:27 +1100

    Update

--- yr12/test.js
+++ yr12/test.js
@@ -1,6 +1,6 @@
-function writefile(myobject)
+function loadwritershell()
 {
-
-
-	document.write(myobject.htmlcontent);
+	document.write("<body>")
+	document.write("<div id=\"writershell\" style=\"display:none;\"><script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbyUUReOeIpiiIJWWMaKORJYURDTso_cm5RNuNu0W0oVKBPfNtdk\/exec\"><\/script><\/div>")
+	document.write("<\/body>")
 }
